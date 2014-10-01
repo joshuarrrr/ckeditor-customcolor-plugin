b068598c2d8ca10c1e9211948c00482ae88f8220
Josh Romero <josh@joshromero.com>
2014-10-01 11:53:57 -0400

    refactor finding insertion point
    now using ranges instead of traversing the dom

--- customcolor/dialogs/customcolor.js
+++ customcolor/dialogs/customcolor.js
@@ -40,7 +40,7 @@ CKEDITOR.dialog.add( 'customcolorDialog', function( editor ) {
             }
             else {
                 CKEDITOR.config.customcolor = newColor;
-                var colorRule = ' .article-detail aside.inlay .sb-hed, .article-detail aside.inlay h3, .article-detail aside.inlay .sb-list-subhead { ' +
+                var colorRule = ' .article-detail aside.inlay .sb-hed, .article-detail aside.inlay h3, .article-detail aside.inlay .sb-list-subhead, .article-detail .listicle li h3, .article-detail .listicle li .listicle-item-hed { ' +
                                 'color: ' +
                                 newColor +'; ' +
                             '} ' +
@@ -50,44 +50,34 @@ CKEDITOR.dialog.add( 'customcolorDialog', function( editor ) {
                             '} ';
 
                 if ( oldStyleBlock ) {
-                    console.log('color already set; resetting now!');
+                    //console.log('color already set; resetting now!');
                     oldStyleBlock.setHtml( colorRule );
                 }
                 else {
                     var newStyleBlock = new CKEDITOR.dom.element( 'style' );
-                    var currentFirstNode;
                     var range = editor.createRange();
-                    
-                    range.moveToPosition( range.root, CKEDITOR.POSITION_BEFORE_START );
-
-                    if ( editor.plugins.divarea ) {
-                        currentFirstNode = range.startContainer.getChildren().getItem( 0 ).getChildren().getItem( 0 );
-                    }
-
-                    else {
-                        /* When using iframe editor, you need to insert in the second (body) node, not the head node of the iframe */
-                        currentFirstNode = range.startContainer.getChildren().getItem( 1 ).getChildren().getItem( 0 );
-                    }
-                    
-                    
-                    //editor.getSelection().selectRanges( [ range ] );
-                    
-                    //console.log( CKEDITOR.dom.children() );
-                    //console.log(CKEDITOR.dom.node);
-                    //console.log(CKEDITOR.dom.nodeList);
 
                     newStyleBlock.setHtml( colorRule );
                     newStyleBlock.setAttribute( 'type', 'text/css' );
                     newStyleBlock.addClass( 'customcolor' );
 
-                    //editor.insertElement( newStyleBlock );
-                    if ( currentFirstNode ) {
-                        newStyleBlock.insertBefore( currentFirstNode );
+                    // No need to do range manipulations if there's nothing in the editor
+                    if ( range.root.$.children.length > 0) {
+                        // the style block will be inserted before any existing elements
+                        range.moveToPosition( range.root, CKEDITOR.POSITION_AFTER_START );
+                        
+                        editor.getSelection().selectRanges( [ range ] );
+
+                        editor.insertElement( newStyleBlock );
+
+                        // after adding the style block, reset cursor to start of editable content
+                        range.moveToPosition( range.getNextEditableNode(), CKEDITOR.POSITION_AFTER_START );
+
+                        editor.getSelection().selectRanges( [ range ] );
                     }
                     else {
-                        editor.insertElement(newStyleBlock);
+                        editor.insertElement( newStyleBlock );
                     }
-
                 }
 
             }
