9d97c4d4eaefa43d1ba27febada31d7be409fb84
Josh Romero <josh@joshromero.com>
2014-10-01 12:34:27 -0400

    make ruleset available as config

--- customcolor/plugin.js
+++ customcolor/plugin.js
@@ -15,3 +15,15 @@ CKEDITOR.plugins.add( 'customcolor', {
         CKEDITOR.dialog.add( 'customcolorDialog', this.path + 'dialogs/customcolor.js' );
     }
 });
+
+/* Can define the ruleset to be affected */
+CKEDITOR.config.customcolor_ruleSet = [
+    {
+        selector: '.article-detail aside.inlay .sb-hed, .article-detail aside.inlay h3, .article-detail aside.inlay .sb-list-subhead, .article-detail .listicle li h3, .article-detail .listicle li .listicle-item-hed',
+        property: 'color'
+    },
+    {
+        selector: '.article-detail aside.inlay',
+        property: 'border-color'
+    }
+];
